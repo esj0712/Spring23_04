36744f0ed42aa11b947f10b781fec2afe70f2cda
82102 <82102@DESKTOP-81LUTCC>
2023-04-03 17:53:38 +0900

    230403

--- spring_basic/src/main/java/kr/co/softsoldesk/main/MainClass2.java
+++ spring_basic/src/main/java/kr/co/softsoldesk/main/MainClass2.java
@@ -4,6 +4,9 @@ import org.springframework.context.support.ClassPathXmlApplicationContext;
 
 import kr.co.softsoldesk.beans.TestBean;
 import kr.co.softsoldesk.beans2.TestBean1;
+import kr.co.softsoldesk.beans2.TestBean2;
+import kr.co.softsoldesk.beans2.TestBean3;
+import kr.co.softsoldesk.beans2.TestBean4;
 
 public class MainClass2 {
 
@@ -14,7 +17,17 @@ public class MainClass2 {
 		TestBean1 t1=ctx.getBean("t1", TestBean1.class);
 		System.out.println("t1"+t1);
 		
+		// #2
+		TestBean2 t2=ctx.getBean("t2", TestBean2.class);
+		System.out.println("t2"+t2);
 		
+		// #3
+		TestBean3 t3=ctx.getBean("t3", TestBean3.class);
+		System.out.println("t3"+t3);
+		
+		// #4
+		TestBean4 t4=ctx.getBean("t4", TestBean4.class);
+		System.out.println("t4"+t4);
 		
 		ctx.close();
 		
